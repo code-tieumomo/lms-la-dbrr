0bfd7c20add1aec10a08a139545e475f726d8cdb
NguyenNgocMinh <minhdev.mochi@gmail.com>
2022-11-16 22:18:11 +0700

    use formik

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,6 +2,9 @@ import  axios  from 'axios';
 import React, { useState } from 'react'
 import { useDispatch } from 'react-redux';
 import { useHistory, useNavigate } from 'react-router-dom';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+
 import exportDefault  from '../redux/actions/ThemeAction';
 import { apiLogin } from '../service/loginApi';
 
@@ -9,60 +12,85 @@ import { ToastContainer, toast } from 'react-toast';
 import jwt_decode from "jwt-decode";
 
 import "./index.css";
-export default function Login() {
 
-    const [userName, setUserName] = useState('');
-    const [password, setPassword] = useState('');
+export default function Login() {
+    const SignupSchema = Yup.object().shape({
+        email: Yup.string().email('Nhap dung dinh dang email').required('Khong de trong'),
+        password: Yup.string()
+        .min(6, 'Mat khau phai lon hon 6 ky tu')
+        .max(15, 'Mat khau khong dai qua 15 ky tu')
+        .required('Khong de trong'),
+    });
+
+    const formik = useFormik({
+        initialValues: {
+            email: '',
+            password: '',
+        },
+        validationSchema: SignupSchema,
+        onSubmit: values => {
+            alert(JSON.stringify(values, null, 2));
+            apiLogin(values)
+            .then((response) => {
+                console.log("res", response.data);
+                if ( !response.data.error ) {
+                    dispatch(exportDefault.updateUser(response.data));
+                    const decoded = jwt_decode(response.data.idToken);
+                    localStorage.setItem('profile-mindx', JSON.stringify(decoded));
+                    localStorage.setItem('accessToken', JSON.stringify(response.data.idToken));
+                    toast.success("Đăng nhập thành công !!!!");
+                    setTimeout(() => {
+                        history('/admin');
+                    }, 1000);
+                } else {
+                    localStorage.removeItem('profile-mindx');
+                    toast.error("Mời bạn nhập đúng tài khoản")
+                }
+            })
+            .catch (error => {
+                toast.error("Mời bạn nhập đúng tài khoản")
+            })
+        },
+    });
 
     let history = useNavigate();
     let dispatch = useDispatch();
-    const handleLogin = (e) => {
-        e.preventDefault();
-        const newUser = {
-            email: userName,
-            password: password,
-        }
-        apiLogin(newUser)
-        .then((response) => {
-            console.log("res", response.data);
-            if ( !response.data.error ) {
-                dispatch(exportDefault.updateUser(response.data));
-                const decoded = jwt_decode(response.data.idToken);
-                localStorage.setItem('profile-mindx', JSON.stringify(decoded));
-                localStorage.setItem('accessToken', JSON.stringify(response.data.idToken));
-                toast.success("Đăng nhập thành công !!!!");
-                setTimeout(() => {
-                    history('/admin');
-                }, 1000);
-            } else {
-                localStorage.removeItem('profile-mindx');
-                toast.error("Mời bạn nhập đúng tài khoản")
-            }
-        })
-        .catch (error => {
-            toast.error("Mời bạn nhập đúng tài khoản")
-        })
-    }
     return (
         <>
-            <div class="login-box">
+            <div className="login-box">
                 <h2>Login</h2>
-                <form>
-                    <div class="user-box">
-                    <input type="text" name="" required="" onChange={ (e) => setUserName(e.target.value) } />
-                    <label>Username</label>
+                <form  onSubmit={formik.handleSubmit}>
+                    <div className="user-box">
+                        <input type="email" name="email" required="" 
+                        onChange={formik.handleChange}
+                        value={formik.values.email}
+                        />
+                        <label htmlFor="Username">Username</label>
+                        {formik.errors.email 
+                        ? ( <div 
+                            className="err__form"
+                            style={{
+                                marginBottom: "20px"
+                            }}
+                        >{formik.errors.email}</div>) : null}
                     </div>
-                    <div class="user-box">
-                    <input type="password" name="" required="" onChange={ (e) => setPassword(e.target.value) }/>
-                    <label>Password</label>
+                    <div className="user-box">
+                        <input type="password" name="password" required="" 
+                        onChange={formik.handleChange}
+                        value={formik.values.password}
+                        />
+                        <label htmlFor="Password">Password</label>
+                        {formik.errors.password ? ( <div className="err__form">{formik.errors.password}</div>) : null}
                     </div>
-                    <a href="#" onClick={ (e) => handleLogin(e) }>
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                        Submit
-                    </a>
+                    <button type="Submit" className="btn__submit">
+                        <a>
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                            Submit
+                        </a>
+                    </button>
                 </form>
             </div>
             <ToastContainer 
